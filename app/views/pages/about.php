f25cde19c81259442f6c68ee9cb1adc07eaf9a13
Kevin Uriel Azuara Fonseca <kebin1421@hotmail.com>
2024-07-16 20:04:09 -0500

    Modified entry message in pages/about view file

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-lg-12">
             <h1>About</h1>
-            <p>This is a social network type app built on the TraversyMVC framework</p>
+            <p>This is a simple authentication app built on the Kira framework</p>
             <p>App Version: <?php echo $data['version']; ?></p>
         </div>
     </div>
